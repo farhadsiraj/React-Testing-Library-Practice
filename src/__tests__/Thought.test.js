72e74383d6c286c43140e6269aded9ad56ce7ce8
Farhad Siraj <farhadsiraj@gmail.com>
2022-05-31 12:04:38 -0400

    Simulated user interactions using the userEvent library

--- src/__tests__/Thought.test.js
+++ src/__tests__/Thought.test.js
@@ -1,11 +1,10 @@
-import "regenerator-runtime/runtime";
 import React from "react";
 import { Thought } from "../Thought.js";
 import { AddThoughtForm } from "../AddThoughtForm.js";
 import { App } from "../App.js";
-import { waitFor, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
+import "regenerator-runtime/runtime";
 import userEvent from "@testing-library/user-event";
 
 test('"Oreos are delicious" should not appear', () => {
@@ -18,11 +17,37 @@ test('"Oreos are delicious" should not appear', () => {
 test("Should show new thought to be present", async () => {
   render(<App />);
 
-  const addThoughtInput = screen.getByRole("input");
-  const addButton = screen.getByRole("submit");
+  const addThoughtInput = screen.getByRole("textbox");
+  const addButton = screen.getByText("Add");
   userEvent.type(addThoughtInput, "Oreos are delicious");
   userEvent.click(addButton);
 
   const thought = await screen.findByText("Oreos are delicious");
+  expect(thought).toBeInTheDocument();
+});
+
+test("Clicking the x button should remove a thought", async () => {
+  render(<App />);
+
+  const button = screen.getAllByText("×")[0];
+
+  userEvent.click(button);
+
+  const removedThought = screen.queryByText(
+    "This is a place for your passing thoughts."
+  );
+  expect(removedThought).toBeNull();
+});
+
+test("Should add a new thought", () => {
+  render(<App />);
+
+  const input = screen.getByRole("textbox");
+  const submit = screen.getByText("Add");
+
+  userEvent.type(input, "Did I forget my keys?");
+  userEvent.click(submit);
+
+  const thought = screen.getByText("Did I forget my keys?");
   expect(thought).toBeInTheDocument();
 });
